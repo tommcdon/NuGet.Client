5efa09e72cba9988ee7c38a6d6312847f86c1567
Ashish Jain (NUGET) <asja@microsoft.com>
2018-07-05 17:12:54 -0700

    Moved wait() to waitAsync() so that we don't block UI thread while waiting for semaphore inside InfiniteScrollListBoxAutomationPeer

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using NuGet.VisualStudio;
 
 namespace NuGet.PackageManagement.UI
 {
@@ -11,19 +12,22 @@ namespace NuGet.PackageManagement.UI
 
         protected override List<AutomationPeer> GetChildrenCore()
         {
-            var infiniteScrollListBox = Owner as InfiniteScrollListBox;
-
-            try
+            return NuGetUIThreadHelper.JoinableTaskFactory.Run(async () =>
             {
-                infiniteScrollListBox?.ItemsLock.Wait();
+                var infiniteScrollListBox = Owner as InfiniteScrollListBox;
 
-                // Don't return the LoadingStatusIndicator as an AutomationPeer, otherwise narrator will report it as an item in the list of packages, even when not visible
-                return base.GetChildrenCore()?.Where(lbiap => !(((ListBoxItemAutomationPeer)lbiap).Item is LoadingStatusIndicator)).ToList() ?? new List<AutomationPeer>();
-            }
-            finally
-            {
-                infiniteScrollListBox?.ItemsLock.Release();
-            }
+                try
+                {
+                    await infiniteScrollListBox?.ItemsLock.WaitAsync();
+
+                    // Don't return the LoadingStatusIndicator as an AutomationPeer, otherwise narrator will report it as an item in the list of packages, even when not visible
+                    return base.GetChildrenCore()?.Where(lbiap => !(((ListBoxItemAutomationPeer)lbiap).Item is LoadingStatusIndicator)).ToList() ?? new List<AutomationPeer>();
+                }
+                finally
+                {
+                    infiniteScrollListBox?.ItemsLock.Release();
+                }
+            });
         }
     }
 }

6a315368c16d98c2d0671fa8e42fad7344933366
Ashish Jain (NUGET) <asja@microsoft.com>
2018-07-16 14:42:08 -0700

    Moved from SemaphoreSlim to ReentrantSemaphore which is JTF aware.

--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBox.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBox.cs
@@ -1,12 +1,18 @@
 using System.Threading;
 using System.Windows.Automation.Peers;
 using System.Windows.Controls;
+using Microsoft.VisualStudio.Threading;
+using NuGet.VisualStudio;
 
 namespace NuGet.PackageManagement.UI
 { 
     internal class InfiniteScrollListBox : ListBox
     {
-        public readonly SemaphoreSlim ItemsLock = new SemaphoreSlim(1, 1);
+        public readonly ReentrantSemaphore ItemsLock =
+            ReentrantSemaphore.Create(
+                initialCount: 1,
+                joinableTaskContext: NuGetUIThreadHelper.JoinableTaskFactory.Context,
+                mode: ReentrantSemaphore.ReentrancyMode.Freeform);
 
         protected override AutomationPeer OnCreateAutomationPeer()
         {
--- src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
+++ src/NuGet.Clients/NuGet.PackageManagement.UI/Xamls/InfiniteScrollListBoxAutomationPeer.cs
@@ -12,21 +12,20 @@ namespace NuGet.PackageManagement.UI
 
         protected override List<AutomationPeer> GetChildrenCore()
         {
+            var infiniteScrollListBox = Owner as InfiniteScrollListBox;
+
+            if (infiniteScrollListBox == null)
+            {
+                return new List<AutomationPeer>();
+            }
+
             return NuGetUIThreadHelper.JoinableTaskFactory.Run(async () =>
             {
-                var infiniteScrollListBox = Owner as InfiniteScrollListBox;
-
-                try
+                await infiniteScrollListBox.ItemsLock.ExecuteAsync(() =>
                 {
-                    await infiniteScrollListBox?.ItemsLock.WaitAsync();
-
                     // Don't return the LoadingStatusIndicator as an AutomationPeer, otherwise narrator will report it as an item in the list of packages, even when not visible
                     return base.GetChildrenCore()?.Where(lbiap => !(((ListBoxItemAutomationPeer)lbiap).Item is LoadingStatusIndicator)).ToList() ?? new List<AutomationPeer>();
-                }
-                finally
-                {
-                    infiniteScrollListBox?.ItemsLock.Release();
-                }
+                });
             });
         }
     }
